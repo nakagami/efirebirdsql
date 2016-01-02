aa725f5a0b3aef93dea407a307e0329ead6407b8
Hajime Nakagami <nakagami@gmail.com>
2016-01-02 21:08:13 +0900

    remove isolation level.

--- include/efirebirdsql.hrl
+++ include/efirebirdsql.hrl
@@ -1,12 +1,6 @@
 %%% The MIT License (MIT)
 %%% Copyright (c) 2016 Hajime Nakagami<nakagami@gmail.com>
 
--define(ISOLATION_LEVEL_READ_COMMITED_LEGACY, 0).
--define(ISOLATION_LEVEL_READ_COMMITED, 1).
--define(ISOLATION_LEVEL_REPEATABLE_READ, 2).
--define(ISOLATION_LEVEL_SERIALIZABLE, 3).
--define(ISOLATION_LEVEL_READ_COMMITED_RO, 4).
-
 -record(column, {
     name :: binary(),
     type :: atom(),
