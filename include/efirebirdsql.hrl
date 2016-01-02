2a269c9ed225f3774e21c9ed9f1276b5f23854f6
Hajime Nakagami <nakagami@gmail.com>
2016-01-02 18:49:07 +0900

    define isolation level.

--- include/efirebirdsql.hrl
+++ include/efirebirdsql.hrl
@@ -1,6 +1,12 @@
 %%% The MIT License (MIT)
 %%% Copyright (c) 2016 Hajime Nakagami<nakagami@gmail.com>
 
+-define(ISOLATION_LEVEL_READ_COMMITED_LEGACY, 0).
+-define(ISOLATION_LEVEL_READ_COMMITED, 1).
+-define(ISOLATION_LEVEL_REPEATABLE_READ, 2).
+-define(ISOLATION_LEVEL_SERIALIZABLE, 3).
+-define(ISOLATION_LEVEL_READ_COMMITED_RO, 4).
+
 -record(column, {
     name :: binary(),
     type :: atom(),
@@ -10,7 +16,7 @@
 
 -record(statement, {
     handle :: binary(),
-    trans_handle :: binary,
+    trans_handle :: pos_integer(),
     name :: string(),
     columns :: [#column{}],
     types :: [atom()]
