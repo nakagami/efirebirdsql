d81add7b353574ec227100e2acbdc8ae59161ebf
Hajime Nakagami <nakagami@gmail.com>
2016-01-22 00:16:09 +0900

    add parse_date(), parse_time() and parse_timestamp()

--- src/efirebirdsql_conv.erl
+++ src/efirebirdsql_conv.erl
@@ -4,7 +4,8 @@
 -module(efirebirdsql_conv).
 
 -export([byte2/1, byte4/2, byte4/1,
-    list_to_xdr_string/1, list_to_xdr_bytes/1 ]).
+    list_to_xdr_string/1, list_to_xdr_bytes/1,
+    parse_date/1, parse_time/1, parse_timestamp/1]).
 
 %%% little endian 2byte
 byte2(N) ->
@@ -53,3 +54,35 @@ list_to_xdr_string(L) ->
 list_to_xdr_bytes(L) ->
     list_to_xdr_string(L).
 
+parse_date(RawValue) ->
+    L = size(RawValue) * 8,
+    <<Num:L>> = RawValue,
+    NDay1 = Num + 678882,
+    Century = ($ * NDay1 -1) div 146097,
+
+    NDay2 = 4 * NDay1 - 1 -  146097 * Century,
+    Day1 = NDay2 div 4,
+
+    NDay3 = (4 * Day1 + 3) div 1461,
+    Day2 = 4 * Day1 + 3 - 1461 * NDay3,
+    Day3 = (Day2 + 4) div 4,
+
+    Month1 = (5 * Day3 - 3) div 153,
+    Day4 = 5 * Day3 - 3 - 153 * Month1,
+    Day5 = (Day4 + 5) div 5,
+    Year1 = 100 * Century + NDay3,
+    Month2 = if Month1 < 10 -> Month1 + 3; true -> Month1 - 9 end,
+    Year2 = if Month1 < 10 -> Year1; true -> Year1 + 1 end,
+    {Year2, Month2, Day5}.
+
+parse_time(RawValue) ->
+    L = size(RawValue) * 8,
+    <<N:L>> = RawValue,
+    S = N div 10000,
+    M = S div 60,
+    H = M div 60,
+    {H, M rem 60, S rem 60, (N rem 10000) * 100}.
+
+parse_timestamp(RawValue) ->
+    <<YMD:4/binary, HMS:4/binary>> = RawValue,
+    {parse_date(YMD), parse_time(HMS)}.
