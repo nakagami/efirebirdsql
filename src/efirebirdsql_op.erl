84d54040cb553f2fa7e8d63aebfe5483a12f1fbb
Hajime Nakagami <nakagami@gmail.com>
2019-01-05 10:10:15 +0000

    to hex auth_data

--- src/efirebirdsql_op.erl
+++ src/efirebirdsql_op.erl
@@ -453,7 +453,7 @@ get_connect_response(_, TcpMod, Sock, State) ->
             AuthData = '',
             _SessionKey = ''
     end,
-    NewState = State#state{accept_version=AcceptVersion, auth_data=binary_to_list(AuthData)},
+    NewState = State#state{accept_version=AcceptVersion, auth_data=efirebirdsql_srp:to_hex(AuthData)},
     {ok, NewState}.
 
 get_connect_response(TcpMod, Sock, State) ->
