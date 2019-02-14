443d418e0b4e2074363c47ac9077358995e4fb22
Hajime Nakagami <nakagami@gmail.com>
2019-02-14 01:46:30 +0000

    fix test

--- test/efirebirdsql_protocol_tests.erl
+++ test/efirebirdsql_protocol_tests.erl
@@ -13,22 +13,21 @@ protocol_test() ->
     {ok, C1} = efirebirdsql_protocol:connect(
         "localhost", "sysdba", "masterkey", tmp_dbname(),
         [{createdb, true}, {auth_plugin, "Srp"}]),
-    {ok, C2} = efirebirdsql_protocol:begin_transaction(true, C1),
-    {ok, C3, Stmt} = efirebirdsql_protocol:allocate_statement(C2),
+    {ok, C2, Stmt} = efirebirdsql_protocol:allocate_statement(C1),
 
-    {ok, C4, Stmt2} = efirebirdsql_protocol:prepare_statement(
-        <<"SELECT rdb$relation_name, rdb$owner_name FROM rdb$relations WHERE rdb$system_flag=?">>, C3, Stmt),
-    {ok, C5, Stmt3} = efirebirdsql_protocol:execute(C4, Stmt2, [1]),
+    {ok, C3, Stmt2} = efirebirdsql_protocol:prepare_statement(
+        <<"SELECT rdb$relation_name, rdb$owner_name FROM rdb$relations WHERE rdb$system_flag=?">>, C2, Stmt),
+    {ok, C4, Stmt3} = efirebirdsql_protocol:execute(C3, Stmt2, [1]),
     _Description = efirebirdsql_protocol:description(Stmt3),
-    {ok, _Rows, C6} = efirebirdsql_protocol:fetchall(C5, Stmt3),
+    {ok, _Rows, C5} = efirebirdsql_protocol:fetchall(C4, Stmt3),
     ?assertEqual(
         efirebirdsql_protocol:columns(Stmt3),
 
         [{<<"RDB$RELATION_NAME">>,text,0,252,true},
          {<<"RDB$OWNER_NAME">>,text,0,252,true}]
     ),
-    {ok, C7} = efirebirdsql_protocol:rollback(C6),
-    {ok, _} = efirebirdsql_protocol:close(C7).
+    {ok, C6} = efirebirdsql_protocol:rollback(C5),
+    {ok, _} = efirebirdsql_protocol:close(C6).
 
 connect_test(_DbName, 0) ->
     ok;
