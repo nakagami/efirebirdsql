66a4f40c95f541eaa382426114e97153c0aff8ae
Hajime Nakagami <nakagami@gmail.com>
2022-09-17 10:54:43 +0900

    add default_charset_test

--- test/efirebirdsql_charset_tests.erl
+++ test/efirebirdsql_charset_tests.erl
@@ -6,6 +6,17 @@
 -include_lib("eunit/include/eunit.hrl").
 -include("efirebirdsql.hrl").
 
+default_charset_test() ->
+    DbName = lists:flatten(io_lib:format("/tmp/~p.fdb", [erlang:system_time()])),
+    {ok, C} = efirebirdsql:connect(
+        "localhost", os:getenv("ISC_USER", "sysdba"), os:getenv("ISC_PASSWORD", "masterkey"), DbName,
+        [{createdb, true}]),
+    ok = efirebirdsql:execute(C, <<"select RDB$CHARACTER_SET_NAME from RDB$DATABASE">>),
+    ?assertEqual({ok, [{<<"RDB$CHARACTER_SET_NAME">>, <<"UTF8">>}]}, efirebirdsql:fetchone(C)),
+
+    ok = efirebirdsql:close(C),
+    ok.
+
 charset_test() ->
     DbName = lists:flatten(io_lib:format("/tmp/~p.fdb", [erlang:system_time()])),
     {ok, C} = efirebirdsql:connect(
